8a2988f591dba8dd1a8102a7b6f0671ee7254737
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-04 14:48:52 +0530

    backend add

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region     = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-kamalb"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"                   
+    encrypt        = true                          
+  }
+}
