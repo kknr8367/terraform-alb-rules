64cf18b003ee0f1ea11502f8c1d5012e5845cff5
Kamalanadha Reddy Kallutla <kamalanadhare.kallu@hcltech.com>
2025-09-04 14:56:34 +0530

    backend & apply

--- providers.tf
+++ providers.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+terraform {
   backend "s3" {
     bucket         = "terraform-kamalb"
     key            = "terraform.tfstate"
